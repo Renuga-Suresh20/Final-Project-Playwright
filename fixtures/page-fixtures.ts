02d07a3cbc3087018fb477e9e3089a6a5d272f4b
Renuga-Suresh20 <renuga.s2@cognizant.com>
2025-10-03 17:38:09 +0530

    fixtures updated

--- fixtures/page-fixtures.ts
+++ fixtures/page-fixtures.ts
@@ -1,7 +1,7 @@
 import { Page } from '@playwright/test';
 import { test as baseTest, expect } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
-import { AdvertisePage } from '../pages/AdvertisePage';
+import { Advertise, AdvertisePage } from '../pages/AdvertisePage';
 import { CarWashPage } from '../pages/CarWashPage';
 import { GymPage } from '../pages/GymPage';
 import { HomeLoanPage } from '../pages/HomeLoanPage';
@@ -9,14 +9,19 @@ import {ServicesPage} from '../pages/ServicesPage';
 
 export class PageFixture {
   readonly homePage: HomePage;
-  readonly searchResultsPage: SearchResultsPage;
-  readonly giftCardPage: GiftCardPage;
-  readonly cartPage: CartPage;
+  readonly advertise: AdvertisePage;
+  readonly carwash: CarWashPage;
+  readonly gym: GymPage;
+  readonly home: HomeLoanPage;
+  readonly services: ServicesPage;
   constructor(page: Page) {
     this.homePage = new HomePage(page);
-    this.searchResultsPage = new SearchResultsPage(page);
-    this.giftCardPage = new GiftCardPage(page);
-    this.cartPage = new CartPage(page);
+    this.advertise = new AdvertisePage(page);
+    this.carwash=new CarWashPage(page);
+    this.gym=new GymPage(page);
+    this.home=new HomeLoanPage(page);
+    this.services=ServicesPage(page);
+    
   }
 }
 type Fixtures = {
