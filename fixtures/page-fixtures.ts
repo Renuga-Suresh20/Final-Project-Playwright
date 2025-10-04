89a0fb93af0733c93547c3bb943de66e1e545f17
Renuga-Suresh20 <renuga.s2@cognizant.com>
2025-10-04 10:41:25 +0530

    updated

--- fixtures/page-fixtures.ts
+++ fixtures/page-fixtures.ts
@@ -1,26 +1,26 @@
 import { Page } from '@playwright/test';
 import { test as baseTest, expect } from '@playwright/test';
-import { HomePage } from '../pages/HomePage';
-import { Advertise, AdvertisePage } from '../pages/AdvertisePage';
-import { CarWashPage } from '../pages/CarWashPage';
-import { GymPage } from '../pages/GymPage';
-import { HomeLoanPage } from '../pages/HomeLoanPage';
-import {ServicesPage} from '../pages/ServicesPage';
+import { Login } from '../pages/HomePage';
+import { Advertise} from '../pages/AdvertisePage';
+import { CarWash } from '../pages/CarWashPage';
+import { Gym } from '../pages/GymPage';
+import { HomeLoan } from '../pages/HomeLoanPage';
+import {Services} from '../pages/ServicesPage';
 
 export class PageFixture {
-  readonly homePage: HomePage;
-  readonly advertise: AdvertisePage;
-  readonly carwash: CarWashPage;
-  readonly gym: GymPage;
-  readonly home: HomeLoanPage;
-  readonly services: ServicesPage;
+  readonly homePage: Login;
+  readonly advertise: Advertise;
+  readonly carwash: CarWash;
+  readonly gym: Gym;
+  readonly home: HomeLoan;
+  readonly services: Services;
   constructor(page: Page) {
-    this.homePage = new HomePage(page);
-    this.advertise = new AdvertisePage(page);
-    this.carwash=new CarWashPage(page);
-    this.gym=new GymPage(page);
-    this.home=new HomeLoanPage(page);
-    this.services=ServicesPage(page);
+    this.homePage = new Login(page);
+    this.advertise = new Advertise(page);
+    this.carwash=new CarWash(page);
+    this.gym=new Gym(page);
+    this.home=new HomeLoan(page);
+    this.services=new Services(page);
     
   }
 }
@@ -33,3 +33,4 @@ export const test = baseTest.extend<Fixtures>({
   },
 });
 export { expect };
+
